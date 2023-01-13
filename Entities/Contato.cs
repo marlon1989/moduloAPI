10d6547cbfa6090dc3709a6bf1c49ea52a4391fc
marlon1989 <marlongabriel1989@gmail.com>
2023-01-13 07:08:04 -0300

    commit inicial

---
+++ Entities/Contato.cs
@@ -0,0 +1,10 @@
+namespace ModuloAPI.Entities
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public String Telefone { get; set; }
+        public bool Ativo {get; set; }
+    }
+}
